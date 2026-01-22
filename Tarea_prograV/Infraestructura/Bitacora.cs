a30da96cdf4b2c3170c8ff301d2d0fad61bbedf2
Bryant Torres Mora <bramora2005@gmail.com>
2026-01-21 21:30:33 -0600

    Se agrego la clase bitacora

---
+++ Tarea_prograV/Infraestructura/Bitacora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_prograV.Infraestructura
+{
+
+    public class Bitacora
+    {
+        private static readonly string NombreArchivo = "bitacora.txt";
+
+        public static void RegistrarEvento(string mensaje)
+        {
+            try
+            {
+                string usuario = Environment.UserName;
+                string fechaHora = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                string linea = $"[{fechaHora}] | Usuario: {usuario} | Evento: {mensaje}";
+
+                File.AppendAllLines(NombreArchivo, new[] { linea });
+            }
+            catch (Exception)
+            {
+                // si falla no se detiene 
+            }
+        }
+
+        public static void RegistrarError(string errorTecnico)
+        {
+            string fechaHora = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string linea = $"[ERROR][{fechaHora}] | Usuario: {Environment.UserName} | Detalle: {errorTecnico}";
+
+            try { File.AppendAllLines(NombreArchivo, new[] { linea }); } catch { }
+        }
+
+
+    }// fin del public
+}// fin name space
